bd5f4b1292b34d3a24f5b3b711f2d2bdde6cb490
sunsetheus <williamsalimrjs@gmail.com>
2024-04-04 09:28:10 +0700

    fix: subscriber model typo

--- src/model/subscriber.rs
+++ src/model/subscriber.rs
@@ -1,12 +1,12 @@
-use rocket::serde::{Deseialize, Serialize};
+use rocket::serde::{Deserialize, Serialize};
 use rocket::log;
 use rocket::serde::json::to_string;
 use rocket::tokio;
-use bambangshop::REQUEST_CLIENT;
-use create::model::notification::Notification;
+use bambangshop::REQWEST_CLIENT;
+use crate::model::notification::Notification;
 
-#[derive(Debug, Clone, Deseialize, Serialize)]
-#[derive(serde(create = "rocket::serde"))]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(create = "rocket::serde")]
 pub struct Subscriber {
     pub url: String,
     pub name: String
